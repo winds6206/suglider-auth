9ea9322a469fa3385bb3f5a55248cc73984d580a
Tony <winds6206@gmail.com>
2023-10-19 14:45:00 +0800

    feat: add cors

--- pkg/api-server/routers.go
+++ pkg/api-server/routers.go
@@ -43,6 +43,18 @@ type CasbinEnforcerConfig = rbac.CasbinEnforcerConfig
 
 func (aa * AuthApiSettings) SetupRouter(swag gin.HandlerFunc) *gin.Engine {
 	router := gin.New()
+
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+			return
+		}
+		c.Next()
+	})
+
 	router.Use(gin.Logger())
 
 	cookieStore := cookie.NewStore([]byte("suglider"))
